b81051eacc3a4723e9d5f3dc07b3b87aaf90710a
Dev Govindji <friendsofdev94@gmail.com>
2019-08-23 20:46:28 -0500

    code serialize and deserialize user functions

--- config/database.js
+++ config/database.js
@@ -3,15 +3,20 @@ var mongoose = require('mongoose');
 mongoose.connect(process.env.DATABASE_URL,
     {
         useNewUrlParser: true,
-        useCreateIndex: true 
     }
 );
 //above object is not necessary
 
-var db = mongoose.connection;
-
-db.on('connected', function() {
-    console.log(`Mongoose connected to ${process.env.DATABASE_URL}`);
+mongoose.connect(`mongodb://localhost/${process.env.DATABASE_URL}`, {
+  useNewUrlParser: true,
+  useCreateIndex: true
 });
 
 module.exports = mongoose;
+
+
+// var db = mongoose.connection;
+
+// db.on('connected', function() {
+//     console.log(`Mongoose connected to ${process.env.DATABASE_URL}`);
+// });
