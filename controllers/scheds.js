24bde2626f5a016661bf1cc77a14d9e98fbc15b3
Dev Govindji <friendsofdev94@gmail.com>
2019-08-28 20:52:12 -0500

    stubs for update functionality and buttons on index page

--- controllers/scheds.js
+++ controllers/scheds.js
@@ -3,12 +3,13 @@ var Sched = require('../models/sched');
 
 module.exports = {
     index,
+    new: newSched,
+    create,
     show,
-    create
+    update
 };
 
 
-//index
 function index(req, res) {
   Sched.find({}, function(Allscheds) {
     res.render('index', {
@@ -18,8 +19,8 @@ function index(req, res) {
   };
 
 
-  function show(req, res) {
-    res.render('scheds/show');
+  function newSched(req, res) {
+    res.render('scheds/new');
     console.log(name);
   }
 
@@ -41,3 +42,32 @@ function index(req, res) {
       );
     });
   }
+
+
+  function show(req, res) {
+    res.render('scheds/show');
+  }
+
+
+  function update(req, res, next) {
+    let { Sched } = req.body
+    Sched.findByIdAndUpdate(req.params.id, {   
+      scheds: [scheds],
+      user: req.user,
+      name: req.name,
+      date: req.date,
+      location: req.location,
+      details: req.details,
+    }, function(err, sched ){
+        if(err) console.error(err);
+        res.redirect(`/scheds/${sched._id}`);
+    });
+}
+
+
+function deleteSched(req, res, next) {
+  Sched.findByIdAndRemove(req.params.id, function(err){
+      Sched.remove();
+  });
+  res.redirect('/scheds');
+}
--- routes/scheds.js
+++ routes/scheds.js
@@ -2,9 +2,11 @@ var express = require('express');
 var router = express.Router();
 var schedsCtrl = require('../controllers/scheds');
 
-router.get('/show', schedsCtrl.show);
-
+router.get('/new', schedsCtrl.new);
 router.post('/index', schedsCtrl.create);
+router.get('/show', schedsCtrl.show);
+router.put('/:id', schedsCtrl.update);
+// router.delete('/:id', schedsCtrl.delete);
 
 function isLoggedIn(req, res, next) {
     if ( req.isAuthenticated() ) return next();
